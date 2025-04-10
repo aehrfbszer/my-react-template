6ae0f89e4eb5180a85e0031d4614bbdc92c69867
mizuki <mizukishuiyue8@gmail.com>
2025-04-10 17:56:43 +0800

    feat: wasm

---
+++ src/pkg/base64_wasm_bg.wasm.d.ts
@@ -0,0 +1,33 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const bytes_to_base64_standard: (
+  a: number,
+  b: number,
+) => [number, number];
+export const bytes_to_urlbase64: (a: number, b: number) => [number, number];
+export const bytes_to_base64: (
+  a: number,
+  b: number,
+  c: number,
+) => [number, number];
+export const base64_to_bytes_standard: (
+  a: number,
+  b: number,
+) => [number, number];
+export const base64_to_bytes_url: (a: number, b: number) => [number, number];
+export const base64_to_bytes_custom: (
+  a: number,
+  b: number,
+  c: number,
+) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_realloc: (
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+) => number;
+export const __wbindgen_start: () => void;
