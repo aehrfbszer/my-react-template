aef76f7f8b19b0cf29fa7c594cbdeb74fb109a56
miya <mizukishuiyue8@gmail.com>
2025-08-18 17:40:32 +0800

    🔧 build(dependencies): update

--- src/api/home.ts
+++ src/api/home.ts
@@ -2,8 +2,8 @@ import { myFetch } from "./myFetch.ts";
 
 export const getHomeList = (params: { fasd: string }) => {
   return myFetch<{ ds: number }>({
-    url: "/api/home/list",
-    method: "GET",
+    url: "/users/all",
+    method: "POST",
     params,
   });
 };
