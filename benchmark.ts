9f053bef37e7fbde66d6b0c5aa29a32efbf52c0a
miya <mizukishuiyue8@gmail.com>
2025-08-19 16:37:38 +0800

    ✨ feat: benchmark

---
+++ benchmark.ts
@@ -0,0 +1,13 @@
+import { bench, do_not_optimize, run } from 'mitata'
+
+const data = 'abcdefg'
+
+bench('index', function* () {
+    yield () => do_not_optimize(data[data.length - 1] === 'q')
+})
+
+bench('startsWith', function* () {
+    yield () => do_not_optimize(data.startsWith('q'))
+})
+
+run()
