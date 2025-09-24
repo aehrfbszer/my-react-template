9f46bd7256cf872fe0127b73df024dee3edfa933
miya <mizukishuiyue8@gmail.com>
2025-09-24 17:13:22 +0800

    🦄 refactor: type

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
   server: {
     port: 3001,
   },
